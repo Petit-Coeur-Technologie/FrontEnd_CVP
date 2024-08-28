cbe2bfbfb5e6c85898b7b3003200970ac11023e4
Ramatoulaye9 <ramatoulayecam9@gmail.com>
2024-08-28 10:05:57 +0000

    Configuration du port pour le déploiement

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: 10000, // Port à vérifier avec Render
-  }
-});
+  server: {
+    port: process.env.PORT || 5173, // Utilise le port défini par Render ou 5173 par défaut
+    host: '0.0.0.0', // Nécessaire pour écouter les connexions externes
+  },
+})
