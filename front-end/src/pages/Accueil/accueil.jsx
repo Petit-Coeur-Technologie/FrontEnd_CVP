35e01a14468651b5cbe1c6dca3cb059d2bc9e2bf
Ramatoulaye9 <ramatoulayecam9@gmail.com>
2024-09-24 20:03:56 +0000

    Défilement de la pagination gérée

--- front-end/src/pages/Accueil/accueil.jsx
+++ front-end/src/pages/Accueil/accueil.jsx
@@ -49,6 +49,10 @@ function Accueil() {
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
+    window.scrollTo({
+      top: 350,
+      behavior: "smooth",
+    });    
   };
 
   // Méthode pour filtrer les PME
