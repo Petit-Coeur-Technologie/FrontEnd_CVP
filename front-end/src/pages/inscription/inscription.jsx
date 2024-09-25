bab6b6d1915d91b82b91477dadbe33d3bcc472fd
diawando <abduldiawade777@gmail.com>
2024-09-25 19:44:16 +0000

    ajout du texte retour à la page d'accueil stylisation pour grand ecran

--- front-end/src/pages/inscription/inscription.jsx
+++ front-end/src/pages/inscription/inscription.jsx
@@ -189,7 +189,7 @@ function Inscription() {
        { /*<img src={background} className="logoInscription" alt="" />*/}
       </div>
       
-      <div className="box-container">
+      <div className="box-container inscriptionForm-wrapper">
         <div className="lg">
           <Link to="/"><img title="Click pour revenir sur l'acceuil" src={myLogo} id="lg" /></Link>
         </div>
@@ -220,9 +220,11 @@ function Inscription() {
           <PmeForm onSubmit={onSubmitPme} isLoading={isLoading} idFileRef={idFileRef} logoFileRef={logoFileRef}/>
         )}
         <div className={`divLogin-link ${userRole === 'menage' ? 'divLogin-linkPme' : ''}`}>
-            <a href='/connexion' className="login-link" >Vous êtes déjà inscrit? Connectez-vous</a>
+           <p> Vous êtes déjà inscrit?  <a href='/connexion' className="login-link" >Connectez-vous</a></p>
         </div>
-
+        <div className="backtohometext_wrapper">
+            <p> <a href="/">Retour à l'Acceuil</a></p>
+        </div>
       </div>
     </div>
   );
