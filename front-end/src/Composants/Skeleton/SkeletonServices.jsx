6c8e6032a005d5badee37cf32b19e05b4a46dbc3
Ramatoulaye9 <ramatoulayecam9@gmail.com>
2024-10-05 21:46:30 +0000

    Skeleton ajouté aux services, skeleton amélioré aux détails

---
+++ front-end/src/Composants/Skeleton/SkeletonServices.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+
+const ServicesLoading = () => {
+    return (
+        <>
+            <div className="service animate-pulse bg-gray-200 p-4 rounded-lg shadow">
+                <img className="h-32 bg-gray-300 rounded-lg mb-4" />
+                <div className='details'>
+                    <h3 className="h-6 bg-gray-300 rounded w-3/4 mb-2"></h3>
+                    <p className="h-4 bg-gray-300 rounded w-5/6"></p>
+                </div>
+            </div>
+
+            <div className="service animate-pulse bg-gray-200 p-4 rounded-lg shadow">
+                <img className="h-32 bg-gray-300 rounded-lg mb-4" />
+                <div className='details'>
+                    <h3 className="h-6 bg-gray-300 rounded w-3/4 mb-2"></h3>
+                    <p className="h-4 bg-gray-300 rounded w-5/6"></p>
+                </div>
+            </div>
+
+            <div className="service animate-pulse bg-gray-200 p-4 rounded-lg shadow">
+                <img className="h-32 bg-gray-300 rounded-lg mb-4" />
+                <div className='details'>
+                    <h3 className="h-6 bg-gray-300 rounded w-3/4 mb-2"></h3>
+                    <p className="h-4 bg-gray-300 rounded w-5/6"></p>
+                </div>
+            </div>
+
+            <div className="service animate-pulse bg-gray-200 p-4 rounded-lg shadow">
+                <img className="h-32 bg-gray-300 rounded-lg mb-4" />
+                <div className='details'>
+                    <h3 className="h-6 bg-gray-300 rounded w-3/4 mb-2"></h3>
+                    <p className="h-4 bg-gray-300 rounded w-5/6"></p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ServicesLoading;
